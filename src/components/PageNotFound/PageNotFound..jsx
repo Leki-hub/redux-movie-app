f66b745790ab140ef8de1e667189a3947a323b56
Leki <lekisintole82@gmail.com>
2023-10-13 14:53:17 +0300

    page 404

--- src/components/PageNotFound/PageNotFound..jsx
+++ src/components/PageNotFound/PageNotFound..jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import notfound from "../../images/pnf.jpg";
 const PageNotFound = () => {
   return (
     <>
-    <h1>PageNotFound</h1>
+ <h1>PageNotFound</h1>
+ <img src={notfound} alt="" />
     
     </>
   ) 
